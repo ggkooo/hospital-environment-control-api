dd24de00eeec91f334fc5141f90d14ccd5cac054
Giordano Bruno Biasi Berwig <giordano.berwig@sou.unijui.edu.br>
2025-10-30 10:41:56 -0300

    feat: created sensor-data minute database migrations

---
+++ database/migrations/2025_10_30_110837_create_m_humidity_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('m_humidity', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('avg_value', 8, 2);
+            $table->decimal('min_value', 8, 2);
+            $table->decimal('max_value', 8, 2);
+            $table->decimal('std_dev', 8, 4)->nullable();
+            $table->integer('reading_count');
+            $table->decimal('variation_range', 8, 2);
+            $table->timestamp('minute_timestamp');
+
+            $table->index('minute_timestamp');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('m_humidity');
+    }
+};
