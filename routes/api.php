dabfcb739fbea54880a10499abd144945124d119
Giordano Bruno Biasi Berwig <giordano.berwig@sou.unijui.edu.br>
2025-10-30 10:45:18 -0300

    fix: updated api routes

--- routes/api.php
+++ routes/api.php
@@ -8,10 +8,39 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Endpoint principal para arrays de dados dos sensores
-Route::post('/sensor-data', [SensorDataController::class, 'store'])
-    ->name('sensor.data.store');
+Route::middleware('api.key')->group(function () {
+    Route::post('/sensor-data', [SensorDataController::class, 'store'])
+        ->name('sensor.data.store');
 
-// Endpoint para compatibilidade com dados únicos
-Route::post('/sensor-data/single', [SensorDataController::class, 'storeSingle'])
-    ->name('sensor.data.store.single');
+    Route::post('/sensor-data/single', [SensorDataController::class, 'storeSingle'])
+        ->name('sensor.data.store.single');
+
+    Route::get('/sensor-data/raw/latest', [SensorDataController::class, 'getLatestData'])
+        ->name('sensor.data.raw.latest');
+
+    Route::get('/sensor-data/raw/all', [SensorDataController::class, 'getAllData'])
+        ->name('sensor.data.raw.all');
+
+    Route::get('/sensor-data/raw/{type}', [SensorDataController::class, 'getData'])
+        ->name('sensor.data.raw.get')
+        ->where('type', 'temperature|humidity|noise|pressure|eco2|tvoc');
+
+    Route::get('/sensor-data/raw/{type}/stats', [SensorDataController::class, 'getStats'])
+        ->name('sensor.data.raw.stats')
+        ->where('type', 'temperature|humidity|noise|pressure|eco2|tvoc');
+
+    Route::get('/sensor-data/minute/all', [SensorDataController::class, 'getAllMinuteData'])
+        ->name('sensor.data.minute.all');
+
+    Route::get('/sensor-data/minute/{type}', [SensorDataController::class, 'getMinuteData'])
+        ->name('sensor.data.minute.get')
+        ->where('type', 'temperature|humidity|noise|pressure|eco2|tvoc');
+
+    Route::get('/sensor-data/minute/{type}/variations', [SensorDataController::class, 'getVariations'])
+        ->name('sensor.data.minute.variations')
+        ->where('type', 'temperature|humidity|noise|pressure|eco2|tvoc');
+
+    Route::get('/sensor-data/minute/{type}/comparison', [SensorDataController::class, 'getComparison'])
+        ->name('sensor.data.minute.comparison')
+        ->where('type', 'temperature|humidity|noise|pressure|eco2|tvoc');
+});
