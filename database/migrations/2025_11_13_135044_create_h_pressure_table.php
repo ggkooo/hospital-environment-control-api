549fd24b287baf539ebae06823ce557e0184d116
Giordano Bruno Biasi Berwig <giordano.berwig@sou.unijui.edu.br>
2025-11-13 11:10:48 -0300

    feat: created sensor-data hour database migrations

---
+++ database/migrations/2025_11_13_135044_create_h_pressure_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('h_pressure', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('avg_value', 8, 2);
+            $table->decimal('min_value', 8, 2);
+            $table->decimal('max_value', 8, 2);
+            $table->decimal('std_dev', 8, 4)->nullable();
+            $table->integer('minute_count');
+            $table->decimal('variation_range', 8, 2);
+            $table->decimal('hourly_trend', 8, 4)->nullable();
+            $table->timestamp('hour_timestamp');
+
+            $table->index('hour_timestamp');
+            $table->unique('hour_timestamp');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('h_pressure');
+    }
+};
