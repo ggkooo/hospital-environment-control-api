353a0441092ce7ec619fa4960c3f36b019281f38
Giordano Bruno Biasi Berwig <giordano.berwig@sou.unijui.edu.br>
2025-11-17 08:38:41 -0300

    fix: updated User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone',
+        'sector',
+        'role',
+        'active',
+        'last_login',
     ];
 
     /**
@@ -41,5 +46,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
+        'active' => 'boolean',
+        'last_login' => 'datetime',
     ];
 }
