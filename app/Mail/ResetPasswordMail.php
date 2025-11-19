4ec14adadc58fc07acb5d69b3815eaf42b4fadf3
Giordano Bruno Biasi Berwig <giordano.berwig@sou.unijui.edu.br>
2025-11-19 10:24:23 -0300

    feat: created the Mail/ResetPasswordMail and SendEmail

---
+++ app/Mail/ResetPasswordMail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ResetPasswordMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $view = 'reset-password';
+
+    public $url;
+    public $user;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($url, $user)
+    {
+        $this->url = $url;
+        $this->user = $user;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Reset Password Notification',
+            to: $this->user->email,
+        );
+    }
+}
